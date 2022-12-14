77adde9586421e7e57e068720baa983ca88b8c54
falamarcao <falamarcao@gmail.com>
2022-12-14 20:17:13 -0300

    167. Lifecycle Methods In Action

--- src/components/User/index.js
+++ src/components/User/index.js
@@ -2,6 +2,10 @@ import { Component } from "react";
 import styles from "./User.module.css";
 
 class User extends Component {
+  componentWillUnmount() {
+    console.log("User.componentWillUnmount");
+  }
+
   render() {
     return <li className={styles.user}>{this.props.name}</li>;
   }
--- src/components/UserFinder/index.jsx
+++ src/components/UserFinder/index.jsx
@@ -1,4 +1,4 @@
-import { Fragment, useState, useEffect } from "react";
+import { Fragment, /*useState, useEffect,*/ Component } from "react";
 import styles from "./UserFinder.module.css";
 
 import Users from "../Users";
@@ -9,31 +9,80 @@ const DUMMY_USERS = [
   { id: "u3", name: "Julie" },
 ];
 
-const UserFinder = () => {
-  const [filteredUsers, setFilteredUsers] = useState(DUMMY_USERS);
-  const [searchTerm, setSearchTerm] = useState("");
+class UserFinder extends Component {
+  //   constructor() {
+  //     super();
+  //     this.state = {
+  //       filteredUsers: [],
+  //       searchTerm: "",
+  //     };
+  //   }
 
-  useEffect(() => {
-    setFilteredUsers(
-      DUMMY_USERS.filter((user) =>
-        user.name.toLowerCase().includes(searchTerm.toLowerCase())
-      )
-    );
-  }, [searchTerm]);
-
-  const searchChangeHandler = (event) => {
-    setSearchTerm(event.target.value);
+  state = {
+    filteredUsers: [],
+    searchTerm: "",
   };
 
-  return (
-    <Fragment>
-      <div className={styles.finder}>
-        <input type="search" onChange={searchChangeHandler} />
-      </div>
+  // On functional components,
+  // we can compare with the use of [useEffect] with no dependencies
+  // or maybe a variable that was created/initialized once the component starts.
+  componentDidMount() {
+    this.setState({ filteredUsers: DUMMY_USERS });
+  }
 
-      <Users users={filteredUsers} />
-    </Fragment>
-  );
-};
+  // On functional components, we can compare with the use of [useEffect] with a dependency array filled.
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.searchTerm !== this.state.searchTerm) {
+      this.setState({
+        filteredUsers: DUMMY_USERS.filter((user) =>
+          user.name.toLowerCase().includes(this.state.searchTerm.toLowerCase())
+        ),
+      });
+    }
+  }
+
+  searchChangeHandler(event) {
+    this.setState({ searchTerm: event.target.value });
+  }
+
+  render() {
+    return (
+      <Fragment>
+        <div className={styles.finder}>
+          <input type="search" onChange={this.searchChangeHandler.bind(this)} />
+        </div>
+
+        <Users users={this.state.filteredUsers} />
+      </Fragment>
+    );
+  }
+}
+
+// const UserFinder = () => {
+//   const [filteredUsers, setFilteredUsers] = useState(DUMMY_USERS);
+//   const [searchTerm, setSearchTerm] = useState("");
+
+//   useEffect(() => {
+//     setFilteredUsers(
+//       DUMMY_USERS.filter((user) =>
+//         user.name.toLowerCase().includes(searchTerm.toLowerCase())
+//       )
+//     );
+//   }, [searchTerm]);
+
+//   const searchChangeHandler = (event) => {
+//     setSearchTerm(event.target.value);
+//   };
+
+//   return (
+//     <Fragment>
+//       <div className={styles.finder}>
+//         <input type="search" onChange={searchChangeHandler} />
+//       </div>
+
+//       <Users users={filteredUsers} />
+//     </Fragment>
+//   );
+// };
 
 export default UserFinder;
